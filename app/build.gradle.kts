57825d019e4569c24dca8d95b467181686011cc2
immoIT <testincome.01@gmail.com>
2026-09-18 12:17:49 +0530

    FIX ANDROID RESOURCE BUILD ERROR

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,27 @@ android {
         targetSdk = prop("TARGET_SDK", "35").toInt()
         versionCode = prop("VERSION_CODE", "1").toInt()
         versionName = prop("VERSION_NAME", "1.0.0")
+
+    buildConfigField("String", "WEB_URL", "\"${prop("WEB_URL", "https://example.com")}\"")
+    buildConfigField("String", "APP_NAME", "\"${prop("APP_NAME", "Website To APK")}\"")
+    buildConfigField("boolean", "ENABLE_JAVASCRIPT", prop("ENABLE_JAVASCRIPT", "true"))
+    buildConfigField("boolean", "ENABLE_DOM_STORAGE", prop("ENABLE_DOM_STORAGE", "true"))
+    buildConfigField("boolean", "ENABLE_PULL_TO_REFRESH", prop("ENABLE_PULL_TO_REFRESH", "false"))
+    buildConfigField("boolean", "ALLOW_EXTERNAL_LINKS", prop("ALLOW_EXTERNAL_LINKS", "true"))
+    buildConfigField("boolean", "ENABLE_DOWNLOADS", prop("ENABLE_DOWNLOADS", "true"))
+    buildConfigField("boolean", "ENABLE_FILE_UPLOAD", prop("ENABLE_FILE_UPLOAD", "true"))
+    buildConfigField("boolean", "ENABLE_FULLSCREEN_VIDEO", prop("ENABLE_FULLSCREEN_VIDEO", "true"))
+    buildConfigField("boolean", "PERMISSION_CAMERA", prop("PERMISSION_CAMERA", "false"))
+    buildConfigField("boolean", "PERMISSION_MICROPHONE", prop("PERMISSION_MICROPHONE", "false"))
+    buildConfigField("boolean", "PERMISSION_LOCATION", prop("PERMISSION_LOCATION", "false"))
+    buildConfigField("boolean", "PERMISSION_NOTIFICATIONS", prop("PERMISSION_NOTIFICATIONS", "false"))
+    buildConfigField("boolean", "PERMISSION_STORAGE", prop("PERMISSION_STORAGE", "false"))
+    buildConfigField("boolean", "PERMISSION_CONTACTS", prop("PERMISSION_CONTACTS", "false"))
+    buildConfigField("boolean", "PERMISSION_PHONE", prop("PERMISSION_PHONE", "false"))
+    buildConfigField("boolean", "PERMISSION_CALENDAR", prop("PERMISSION_CALENDAR", "false"))
+    buildConfigField("boolean", "PERMISSION_BLUETOOTH", prop("PERMISSION_BLUETOOTH", "false"))
+    buildConfigField("boolean", "PERMISSION_VIBRATE", prop("PERMISSION_VIBRATE", "false"))
+    buildConfigField("boolean", "PERMISSION_NFC", prop("PERMISSION_NFC", "false"))
     }
 
     buildFeatures {
@@ -43,26 +64,6 @@ android {
         jvmTarget = "17"
     }
 
-    buildConfigField("String", "WEB_URL", ""${prop("WEB_URL", "https://example.com")}"")
-    buildConfigField("String", "APP_NAME", ""${prop("APP_NAME", "Website To APK")}"")
-    buildConfigField("boolean", "ENABLE_JAVASCRIPT", prop("ENABLE_JAVASCRIPT", "true"))
-    buildConfigField("boolean", "ENABLE_DOM_STORAGE", prop("ENABLE_DOM_STORAGE", "true"))
-    buildConfigField("boolean", "ENABLE_PULL_TO_REFRESH", prop("ENABLE_PULL_TO_REFRESH", "false"))
-    buildConfigField("boolean", "ALLOW_EXTERNAL_LINKS", prop("ALLOW_EXTERNAL_LINKS", "true"))
-    buildConfigField("boolean", "ENABLE_DOWNLOADS", prop("ENABLE_DOWNLOADS", "true"))
-    buildConfigField("boolean", "ENABLE_FILE_UPLOAD", prop("ENABLE_FILE_UPLOAD", "true"))
-    buildConfigField("boolean", "ENABLE_FULLSCREEN_VIDEO", prop("ENABLE_FULLSCREEN_VIDEO", "true"))
-    buildConfigField("boolean", "PERMISSION_CAMERA", prop("PERMISSION_CAMERA", "false"))
-    buildConfigField("boolean", "PERMISSION_MICROPHONE", prop("PERMISSION_MICROPHONE", "false"))
-    buildConfigField("boolean", "PERMISSION_LOCATION", prop("PERMISSION_LOCATION", "false"))
-    buildConfigField("boolean", "PERMISSION_NOTIFICATIONS", prop("PERMISSION_NOTIFICATIONS", "false"))
-    buildConfigField("boolean", "PERMISSION_STORAGE", prop("PERMISSION_STORAGE", "false"))
-    buildConfigField("boolean", "PERMISSION_CONTACTS", prop("PERMISSION_CONTACTS", "false"))
-    buildConfigField("boolean", "PERMISSION_PHONE", prop("PERMISSION_PHONE", "false"))
-    buildConfigField("boolean", "PERMISSION_CALENDAR", prop("PERMISSION_CALENDAR", "false"))
-    buildConfigField("boolean", "PERMISSION_BLUETOOTH", prop("PERMISSION_BLUETOOTH", "false"))
-    buildConfigField("boolean", "PERMISSION_VIBRATE", prop("PERMISSION_VIBRATE", "false"))
-    buildConfigField("boolean", "PERMISSION_NFC", prop("PERMISSION_NFC", "false"))
 }
 
 dependencies {
